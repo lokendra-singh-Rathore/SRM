203dd5b0676052a063426b68dc89e94cf06165db
lokendra-singh-Rathore <80844824+lokendra-singh-Rathore@users.noreply.github.com>
2021-11-29 10:23:00 +0530

    Update settings.py

--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['iarlokendra.herokuapp.com/']
+ALLOWED_HOSTS = ['iarlokendra.herokuapp.com']
 
 
 # Application definition
