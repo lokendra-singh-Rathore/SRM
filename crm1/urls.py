3461eb233f4eb8efa659c69db48af797d088ff21
lokendra-singh-Rathore <80844824+lokendra-singh-Rathore@users.noreply.github.com>
2021-11-29 10:24:00 +0530

    Update urls.py

--- crm1/urls.py
+++ crm1/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('acc', include('accounts.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS}), 
+	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
